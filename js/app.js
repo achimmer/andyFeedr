20f9561dddd9e064a64c61efcf4cd23ec5ce442a
Andrew Himmer <achimmer@gmail.com>
2016-08-15 22:46:55 -0500

    slight change

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 $(function() {
-  var $source1Btn = $('#source1');
-  var $source2Btn = $('#source2');
+  var $source1Btn = $('.source1');
+  var $source2Btn = $('.source2');
   var $feedContent = $('#feedContent');
   var $modalContent = $('#modalContent');
   var $preLoader = $('#loading');
@@ -36,7 +36,7 @@ $(function() {
           var tumblrLink = "<a href='" + tumblrUrl + "' target='_blank'>Read More<a>";
           var linkUrl = $(this).find("link_url").text();
           var link = "<a href='" + linkUrl + "' target='_blank'>Read More<a>";
-          var feedContent1 = "<article class='col s4 card-panel post'><h1 class='postTitle truncate'>" + title + "</h1><p class='postDescription'>" + description + linkUrl + "</p>" + "<p>" + tumblrLink + "</p>" + "</article>";
+          var feedContent1 = "<article class='col s12 m6 l4 card-panel post'><h1 class='postTitle truncate'>" + title + "</h1><p class='postDescription'>" + description + linkUrl + "</p>" + "<p>" + tumblrLink + "</p>" + "</article>";
           $feedContent.append(feedContent1);
         });
         $preLoader.hide();
@@ -62,7 +62,7 @@ $(function() {
           var wikiUrl = $(this).find("id").text();
           var wikiLink = "<a href='" + wikiUrl + "' target='_blank'>Read More<a>";
           var modalContent2 = "<h1 class='postTitle truncate'>" + title + "</h1><div class='postDescription'>" + summary + "</div><p>"+ wikiLink + "</p>";
-          var feedContent2 = "<article class='col s4 card-panel post fixed-height'><h1 class='postTitle truncate'>" + title + "</h1><div class='postDescription'>" + summary + "</div><p>"+ wikiLink + "</p></article>";
+          var feedContent2 = "<article class='col s12 m6 l4 card-panel post fixed-height'><h1 class='postTitle truncate'>" + title + "</h1><div class='postDescription'>" + summary + "</div><p>"+ wikiLink + "</p></article>";
           $feedContent.append(feedContent2);
           $modalContent.append(modalContent2);
         });
